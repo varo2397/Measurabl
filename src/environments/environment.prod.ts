23b37bdd9c6b93aebb46c80b8fcd31c7e9733327
Alvaro Castro <alvarocastro74@gmail.com>
2021-12-13 02:56:39 -0600

    changed base

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://5c37c33f7820ff0014d927c5.mockapi.io/msr'
+  BASE_URL: 'https://5c37c33f7820ff0014d927c5.mockapi.io/msr'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://5c37c33f7820ff0014d927c5.mockapi.io/msr'
+  BASE_URL: 'https://5c37c33f7820ff0014d927c5.mockapi.io/msr'
 };
 
 /*
